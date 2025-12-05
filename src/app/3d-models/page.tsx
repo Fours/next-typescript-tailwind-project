e0a948e02daad8a420deebd51b5b93cff0b242df
Kyle Willett <kylewillett@yahoo.com>
2025-12-05 13:10:45 -0600

    Added filter to models page

--- src/app/3d-models/page.tsx
+++ src/app/3d-models/page.tsx
@@ -1,7 +1,36 @@
 import ModelsGrid from "@/app/components/ModelsGrid"
 import { getModels } from "@/app/lib/models"
 
-export default async function Page() {
-    const models = await getModels()
-    return <ModelsGrid title="3D Models" models={models} />
+type ModelsPageProps = {
+    searchParams: Promise<{
+        q?: string
+    }>
+}
+
+export default async function ModelsPage({ searchParams }: ModelsPageProps) {
+
+    const query = (await searchParams).q
+    let models = await getModels()
+    if (query) {
+        models = models.filter(model => {
+            return model.name.toLowerCase().includes(query.toLowerCase()) ||
+                model.description.toLowerCase().includes(query.toLowerCase())
+        })
+    }
+
+    return (
+        <>
+            <form className="w-full px-5 md:px-0 md:max-w-xl">
+                <input
+                    type="text"
+                    name="q"
+                    placeholder="E.g. dragon"
+                    autoComplete="off"
+                    defaultValue={query}
+                    className="w-full py-3 px-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
+                />
+            </form>
+            <ModelsGrid title="3D Models" models={models} />
+        </>
+    )
 }
