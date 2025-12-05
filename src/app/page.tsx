fc66695acdb032ba4268a4d6dacc3d7a7d6da876
Kyle Willett <kylewillett@yahoo.com>
2025-12-05 13:23:00 -0600

    Fixed button on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,12 @@ export default function Home() {
           </p>
 
           <div className="flex gap-4">
-            <button
+            <a
               href="/3d-models"
               className="px-6 py-3 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
             >
               Browse Models
-            </button>
+            </a>
           </div>
         </div>
         <Image 
